4c454a2bc582dd40929c842e0feebaaf55b42376
Mariano-CE <marianomm1301@outlook.com>
2018-10-24 19:41:53 -0600

    Listo SFML

--- cmake-build-debug/CMakeFiles/Proyecto2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Proyecto2.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Proyecto2.dir/main.cpp.o"
-  "Proyecto2.pdb"
-  "Proyecto2"
+  "../bin/Proyecto2.pdb"
+  "../bin/Proyecto2"
 )
 
 # Per-language clean rules from dependency scanning.
